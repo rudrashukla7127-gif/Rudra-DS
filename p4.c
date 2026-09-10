cded96c92b8e1cc32fa30e522581c00582b31038
rudrashukla7127-gif <rudrashukla7127@gmail.com>
2026-09-10 10:30:10 +0530

    Implement postfix expression evaluation in C

---
+++ p4.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define MAX 100
+
+int stack[MAX];
+int top = -1;
+
+void push(int value)
+{
+    stack[++top] = value;
+}
+
+int pop()
+{
+    return stack[top--];
+}
+
+int main()
+{
+    char postfix[MAX];
+    int i, a, b, result;
+
+    printf("Enter postfix expression: ");
+    scanf("%s", postfix);
+
+    for (i = 0; postfix[i] != '\0'; i++)
+    {
+        if (isdigit(postfix[i]))
+        {
+            push(postfix[i] - '0');
+        }
+        else
+        {
+            b = pop();
+            a = pop();
+
+            switch (postfix[i])
+            {
+                case '+':
+                    result = a + b;
+                    break;
+
+                case '-':
+                    result = a - b;
+                    break;
+
+                case '*':
+                    result = a * b;
+                    break;
+
+                case '/':
+                    result = a / b;
+                    break;
+
+                case '%':
+                    result = a % b;
+                    break;
+
+                case '^':
+                    result = 1;
+                    for (int j = 0; j < b; j++)
+                    {
+                        result *= a;
+                    }
+                    break;
+            }
+
+            push(result);
+        }
+    }
+
+    printf("Result = %d\n", pop());
+
+    return 0;
+}
